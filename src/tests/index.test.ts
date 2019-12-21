1b384ea322027346ea4d676befdfa9c9b66b4121
Jonathan Hattab <jonathan@madjoh.com>
2019-12-21 13:21:04 +0100

    Created mnm template.

---
+++ src/tests/index.test.ts
@@ -0,0 +1,11 @@
+import * as mocha from 'mocha';
+import * as chai  from 'chai';
+import * as sinon from 'sinon';
+
+import { SERVICE } from '../index';
+
+mocha.describe('SERVICE', function(){
+	mocha.it('No tests written yet', function(){
+		chai.expect(true).to.be.true;
+	});
+});
